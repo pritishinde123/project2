f1614466ab882898ba55145b86c53967aa3f28c9
Administrator <Administrator@PCNO-4>
2022-02-01 20:06:43 +0530

    Add project files.

---
+++ encapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace encapsulation
+{
+    // C# program to illustrate encapsulation
+
+    public class BaseClass
+    {
+        protected internal int myValue = 0;
+    }
+
+
+    class DerivedClass : BaseClass
+    {
+        static void Main()
+        {
+
+            var derivedObject = new DerivedClass();
+
+            // Error CS1540, because myValue can only be accessed by
+            // classes derived from BaseClass.
+            // baseObject.myValue = 10;
+
+            // OK, because this class derives from BaseClass.
+            derivedObject.myValue = 10;
+        }
+    }
+}
